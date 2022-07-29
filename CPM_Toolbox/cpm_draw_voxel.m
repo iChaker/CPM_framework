ee108e8e6d94f220de287c812e684cca79b52b4c
SRSteinkamp <simon.steinkamp@googlemail.com>
2022-07-29 13:29:37 +0200

    new function to visualize grid over parameters, a slightly adjusted form of recovery graph, using subplots instead of tiles, to be a bit more explicit in code

--- CPM_Toolbox/cpm_draw_voxel.m
+++ CPM_Toolbox/cpm_draw_voxel.m
@@ -65,20 +65,20 @@ for i=1:length(p_names)
 
 end
 
-try tile;
-   nexttile(tile); 
-   c=9;
-catch
-   figure;
-   c=17;
-end
+% try tile;
+%    nexttile(tile); 
+%    c=9;
+% catch
+%    figure;
+c=0;
+% end
 z  = feval(PRF.M.IS, 	P , PRF.M, PRF.U, 'get_response', coords);
 z  = reshape(z,b,b);
 
 ticks = linspace(1,b,c);
 
 contourf(peaks)
-colormap(jet);
+% colormap(jet);
 imagesc(z);
 %[ 'voxel:' num2str(voxel) 'at' num2str(fixed_values)]
 if c==17
