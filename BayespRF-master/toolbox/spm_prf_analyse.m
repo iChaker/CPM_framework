281eeabf8ad2d98f997ef269b362f753109ac6f9
SRSteinkamp <simon.steinkamp@googlemail.com>
2022-07-29 13:42:55 +0200

    Revert "changed check to not be overly sensitive"
    This reverts commit 9f82d6c3f5b67258e66c3966d64dd8cee82fe65d.

--- BayespRF-master/toolbox/spm_prf_analyse.m
+++ BayespRF-master/toolbox/spm_prf_analyse.m
@@ -354,7 +354,7 @@ if ~floor(bins_per_TR)==bins_per_TR
     warning('Bins per TR is not an integer value, please check dt and TR input values');
     bins_per_TR = round(bins_per_TR);
 
-elseif mod(bins_per_TR, 1) ~= 0
+elseif class(bins_per_TR) == 'double'
 
     warning('Bins per TR is not an integer value, please check dt and TR input values');
     bins_per_TR = round(bins_per_TR);
